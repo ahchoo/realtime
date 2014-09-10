ac87df94bd00c138343cf6d7b78c1d936a522de3
fuqcool <fuqcool@gmail.com>
2014-09-10 13:53:26 -0400

    auth test

--- test/routes/auth.spec.js
+++ test/routes/auth.spec.js
@@ -2,104 +2,107 @@
 
 var expect = require('expect.js')
 var request = require('supertest')
+var mockgoose = require('mockgoose')
+var mongoose = require('mongoose')
 var md5 = require('MD5')
 
-describe('Routes test', function () {
-  describe('Auth', function () {
-    var app
-    var email = 'test@ahchoo.com'
-    var password = md5('husky')
+describe('auth api', function () {
 
-    before(function (done) {
-      require('mockgoose')(require('mongoose'))
-      require('../../fixture')().then(function () {
-        require('mongoose').disconnect(function () {
-          app = require('../../app')
-          done()
-        })
+  var app
+
+  before(function (done) {
+    mockgoose(mongoose)
+
+    require('../../lib/connect-db')().then(function () {
+      app = require('../../app')
+
+      var models = require('../../lib/models')
+      models.User.create({
+        email: 'test@ahchoo.com',
+        password: md5('123'),
+        name: 'Ahchoo Tech',
+        token: 'abc'
+      }, function (err, user) {
+        done()
       })
     })
 
-    after(function () {
-      require('mongoose').disconnect()
-    })
-
-    describe('POST /api/auth', function () {
-      it('expect login success with correct username and password', function (done) {
-        request(app)
-          .post('/api/auth')
-          .type('form')
-          .send({
-            email: email,
-            password: password
-          })
-          .end(function (err, res) {
-            expect(err).to.not.be.ok()
-            expect(res.body.data).to.be.ok()
-            expect(res.body.data.email).to.be(email)
-            expect(res.body.data.token).to.be.ok()
-            done()
-          })
-      })
-
-      it('expect login fail with wrong password', function (done) {
-        request(app)
-          .post('/api/auth')
-          .type('form')
-          .send({
-            email: email,
-            password: 'stub password'
-          })
-          .end(function (err, res) {
-            expect(err).to.not.be.ok()
-            expect(res.body.error).to.be.ok()
-            expect(res.body.error.message).to.be.ok()
-            done()
-          })
-      })
-
-      it('expect login fail with wrong username and password', function (done) {
-        request(app)
-          .post('/api/auth')
-          .type('form')
-          .send({
-            email: 'stub username',
-            password: 'stub password'
-          })
-          .end(function (err, res) {
-            expect(err).to.not.be.ok()
-            expect(res.body.error).to.be.ok()
-            expect(res.body.error.message).to.be.ok()
-            done()
-          })
-      })
-
-    })
-
-    // describe('PUT /api/auth', function () {
-    //   it('expect create a user', function (done) {
-    //     var email = 'stub_test@test.com'
-    //     var name = 'stub name'
-    //     var password = md5('stub password')
-    //     var password2 = password
-
-    //     request(app)
-    //       .put('/api/auth')
-    //       .type('form')
-    //       .send({
-    //         email: email,
-    //         name: name,
-    //         password: password,
-    //         password2: password2
-    //       })
-    //       .end(function (err, res) {
-    //         expect(err).to.not.be.ok()
-    //         expect(res.body.data).to.be.ok()
-    //         expect(res.body.data.email).to.be(email)
-    //         expect(res.body.data.token).to.be.ok()
-    //         done()
-    //       })
-    //   })
-    // })
   })
+
+  after(function () {
+    mockgoose.reset()
+  })
+
+  it('should login successfully', function (done) {
+    request(app)
+      .post('/api/auth')
+      .type('form')
+      .send({
+        email: 'test@ahchoo.com',
+        password: md5('123')
+      })
+      .end(function (err, res) {
+        expect(res.body).to.eql({
+          data: {
+            email: 'test@ahchoo.com',
+            name: 'Ahchoo Tech',
+            token: 'abc'
+          }
+        })
+
+        done()
+      })
+  })
+
+  it('should fail to login if there no such account exists', function (done) {
+    request(app)
+      .post('/api/auth')
+      .type('form')
+      .send({
+        email: 'foo@bar.com',
+        password: md5('abcd')
+      })
+      .end(function (err, res) {
+        expect(res.body).to.eql({
+          error: {message: 'Unmatched email and password'}
+        })
+
+        done()
+      })
+  })
+
+  it('should fail to login if email is not correct', function (done) {
+    request(app)
+      .post('/api/auth')
+      .type('form')
+      .send({
+        email: 'test@ahchoo.com',
+        password: md5('abcd')
+      })
+      .end(function (err, res) {
+        expect(res.body).to.eql({
+          error: {message: 'Unmatched email and password'}
+        })
+
+        done()
+      })
+  })
+
+  it('should fail to login if password is not correct', function (done) {
+    request(app)
+      .post('/api/auth')
+      .type('form')
+      .send({
+        email: 'foo@bar.com',
+        password: md5('123')
+      })
+      .end(function (err, res) {
+        expect(res.body).to.eql({
+          error: {message: 'Unmatched email and password'}
+        })
+
+        done()
+      })
+  })
+
 })
