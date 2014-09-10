318d96c8fb7415ce41adc2db045886f16db65731
maizhiying <zhiying.mai@gmail.com>
2014-09-10 15:35:08 +0800

    ssl demo

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 #!/bin/env node
 
+var fs = require('fs')
 var http = require('http')
+var https = require('https')
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf-8')
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf-8')
 var app = require('./app')
 var server = http.createServer(app)
+var httpsServer = https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app)
+
 server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,
               process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
               function () {
                 console.log('Server started')
               })
+
+httpsServer.listen(8081, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function () {
+  console.log('Https Server started')
+})
 
 require('./lib/socket')(server, app.sessionStore)
