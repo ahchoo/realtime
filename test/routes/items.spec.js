4f1dd3f9b9cd830200861827b60003964dc4c036
fuqcool <fuqcool@gmail.com>
2014-09-10 22:05:27 -0400

    tests for item

--- test/routes/items.spec.js
+++ test/routes/items.spec.js
@@ -3,21 +3,28 @@
 var expect = require('expect.js')
 var request = require('supertest')
 var mockgoose = require('mockgoose')
+var ObjectId = require('mongoose').Types.ObjectId
 var _ = require('underscore')
 var models = require('../../lib/models')
 
 describe('Items test', function () {
   var app
+  var ids = {}
 
   beforeEach(function (done) {
     app = require('../../app')
 
+    ids.iphone = ObjectId()
+    ids.burger = ObjectId()
+
     models.Item.create({
+      _id: ids.iphone,
       title: 'iPhone 6',
       description: 'bigger than bigger',
       price: 750
     }).then(function () {
       return models.Item.create({
+        _id: ids.burger,
         title: 'Burger',
         description: 'tasty',
         price: 8
@@ -40,12 +47,11 @@ describe('Items test', function () {
         price: 150
       })
       .end(function (err, res) {
-        expect(res.body.error).to.not.be.ok()
+        var item = res.body.data
 
-        expect(_.pick(res.body.data, 'title', 'price')).to.eql({
-          title: 'Filco Blue',
-          price: 150
-        })
+        expect(item._id).to.be.ok()
+        expect(item.title).to.be('Filco Blue')
+        expect(item.price).to.be(150)
 
         models.Item.find().exec().then(function (items) {
           expect(items.length).to.be(3)
@@ -62,63 +68,59 @@ describe('Items test', function () {
 
         expect(items.length).to.be(2)
 
-        var item1 = _.find(items, function (item) {
-          return item.title === 'iPhone 6'
+        var iphone = _.find(items, function (item) {
+          return item._id === ids.iphone.toString()
         })
 
-        expect(_.pick(item1, 'title', 'description', 'price')).to.eql({
-          title: 'iPhone 6',
-          description: 'bigger than bigger',
-          price: 750
+        expect(iphone._id).to.be(ids.iphone.toString())
+        expect(iphone.title).to.be('iPhone 6')
+        expect(iphone.description).to.be('bigger than bigger')
+        expect(iphone.price).to.be(750)
+
+        var burger = _.find(items, function (item) {
+          return item._id === ids.burger.toString()
         })
 
-        var item2 = _.find(items, function (item) {
-          return item.title === 'Burger'
-        })
+        expect(burger._id).to.be(ids.burger.toString())
+        expect(burger.title).to.be('Burger')
+        expect(burger.description).to.be('tasty')
+        expect(burger.price).to.be(8)
 
-        expect(_.pick(item2, 'title', 'description', 'price')).to.eql({
-          title: 'Burger',
-          description: 'tasty',
-          price: 8
-        })
+        done()
+      })
+  })
+
+  it('should get an item with id', function (done) {
+    request(app)
+      .get('/api/items/' + ids.iphone.toString())
+      .end(function (err, res) {
+        var iphone = res.body.data
+
+        expect(iphone._id).to.be(ids.iphone.toString())
+        expect(iphone.title).to.be('iPhone 6')
+        expect(iphone.description).to.be('bigger than bigger')
+        expect(iphone.price).to.be(750)
 
         done()
       })
   })
 
 
-  // it('should remove an item', function (done) {
-  //   models.
+  it('should remove an item', function (done) {
+    request(app)
+      .delete('/api/items/' + ids.iphone.toString())
+      .end(function (err, res) {
+        var item = res.body.data
 
-  //   request(app)
-  //     .get('/api/items/' + )
-  //     .end(function (err, res) {
+        expect(item).to.be.ok()
 
+        models.Item.find().exec().then(function (items) {
+          expect(items.length).to.be(1)
+          expect(items[0].id).to.be(ids.burger.toString())
 
-  //       done()
-  //     })
-  // })
+          done()
+        })
+      })
+  })
 
-  // describe('DELETE /api/items/:itemId', function () {
-  //   it('expect remove a specific item', function (done) {
-  //     expect(itemId).to.be.ok()
-  //     request(app)
-  //       .delete('/api/items/' + itemId)
-  //       .end(function (err, res) {
-  //         expect(err).to.not.be.ok()
-  //         expect(res.body.data).to.be.ok()
-  //         expect(res.body.data.title).to.be('stub title')
-  //         expect(res.body.data.price).to.be(111)
-
-  //         request(app)
-  //           .get('/api/items/')
-  //           .end(function (err, res) {
-  //             expect(err).to.not.be.ok()
-  //             expect(res.body.data).to.be.ok()
-  //             expect(res.body.data.length).to.be(2)
-  //             done()
-  //           })
-  //       })
-  //   })
-  // })
 })
