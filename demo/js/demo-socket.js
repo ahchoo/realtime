57e02685a34bac22d8021262509c4a7ccf153c32
maizhiying <zhiying.mai@gmail.com>
2014-09-15 15:50:05 +0800

    delete demo

--- demo/js/demo-socket.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  var socket = io.connect()
-
-  socket.on('connect_failed', function (reason) {
-    console.error('Unable to Connect: ', reason)
-  }).on('connect', function () {
-    console.log('Connect success.')
-  })
-})
