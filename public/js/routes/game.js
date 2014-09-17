4becf3f9fb0d2b0f3aa1f8189006378a753d6371
fuqcool <fuqcool@gmail.com>
2014-09-16 22:32:24 -0400

    now we have countdown timer

--- public/js/routes/game.js
+++ public/js/routes/game.js
@@ -5,50 +5,108 @@ module.exports = function (el) {
   var cookie = require('cookie-cutter')
   var _ = require('underscore')
   var window = require('global/window')
+  var q = require('q')
+  var $ = require('jquery-browserify')
 
   var api = require('../lib/api')
   var router = require('../lib/router')
 
-  var users = ko.observableArray()
+  function GameView(gameId) {
+    var self = this
 
-  var socket
-  var query = 'token=' + cookie.get('ahchoo_token') + '&gameId=' + router.params.gameId
+    this.socket = null
+    this.gameId = gameId
+    this.timeRemaining = 100
+    this.started = false
+    this.users = ko.observableArray()
 
-  var currentUri = window.location.href
+    this.connectSocket()
+    this.socket.on('player-joined', function (user) {
+      self.addUser(user)
+    })
+    this.socket.on('players-joined', function (users) {
+      self.addUserCollection(users)
+    })
+    this.socket.on('player-left', function (user) {
+      self.removeUser(user)
+    })
 
-  // need different config for local and production env
-  if (currentUri.indexOf('127.0.0.1') !== -1 ||
-      currentUri.indexOf('localhost') !== -1) {
-    socket = io.connect('127.0.0.1', {query: query})
-  } else {
-    socket = io.connect('ws://realtime-ahchoo.rhcloud.com:8000', {query: query})
-  }
+    this.itemName = ko.observable()
 
-  socket.on('player-joined', addUser)
-  socket.on('players-joined', addUserCollection)
-  socket.on('player-left', removeUser)
+    setTimeout(function () { self.reset() }, 3000)
 
-  function addUser(user) {
-    users.push(user)
-  }
-
-  function addUserCollection(users) {
-    _.forEach(users, function (user) {
-      addUser(user)
+    api.game.one({id: gameId}).then(function (game) {
+      self.itemName(game.item.title)
     })
   }
 
-  function removeUser(target) {
-    users.remove(function (user) {
+  GameView.prototype.start = function () {
+    var self = this
+
+    this.tId = setTimeout(function () {
+      self.timeRemaining -= 1
+      self.updateTime()
+
+      if (self.timeRemaining > 0) {
+        self.start()
+      }
+    }, 100)
+  }
+
+  GameView.prototype.updateTime = function () {
+    $('#time_remaining').text(String((this.timeRemaining / 10).toFixed(1)))
+  }
+
+  GameView.prototype.reset = function () {
+    if (this.tId) {
+      clearTimeout(this.tId)
+    }
+
+    this.timeRemaining = 100
+    this.start()
+  }
+
+  GameView.prototype.over = function () {
+    clearInterval(this.intervalId)
+  }
+
+  GameView.prototype.addUser = function (user) {
+    this.users.push(user)
+  }
+
+  GameView.prototype.addUserCollection = function (users) {
+    var self = this
+
+    _.forEach(users, function (user) {
+      self.addUser(user)
+    })
+  }
+
+  GameView.prototype.removeUser = function (target) {
+    this.users.remove(function (user) {
       return user._id === target._id
     })
   }
 
-  api.game.one({id: router.params.gameId}).then(function (game) {
-    ko.applyBindings({
-      name: game.item.title,
-      users: users
-    }, el)
-  })
+  GameView.prototype.connectSocket = function () {
+    var query = 'token=' + cookie.get('ahchoo_token') + '&gameId=' + this.gameId
+
+    var currentUri = window.location.href
+
+    // need different config for local and production env
+    if (currentUri.indexOf('127.0.0.1') !== -1 ||
+        currentUri.indexOf('localhost') !== -1) {
+      this.socket = io.connect('127.0.0.1', {query: query})
+    } else {
+      this.socket = io.connect('ws://realtime-ahchoo.rhcloud.com:8000', {query: query})
+    }
+  }
+
+  var game = new GameView(router.params.gameId)
+
+  ko.applyBindings(game, el)
+
+  // ko.applyBindings(new GameView(router.params.gameId), el)
+  // ko.applyBindings({itemName: 'adfksadfhksadhjf'}, el)
 
 }
